26059f58c2e19832a2fd60fac2f704745e18cf6e
matjazsu <matjaz.suber@gmail.com>
2013-12-26 15:46:54 +0100

    Add frame for WumpusPlayer.

--- Wumpus/src/WumpusMain.java
+++ Wumpus/src/WumpusMain.java
@@ -14,8 +14,8 @@ public class WumpusMain {
 	public static void main(String[] args) {
 
 		//Read from input file
-		String file = args[0];
-		//String file = "/Users/matjaz/Desktop/svet_wumpusa.txt";
+		//String file = args[0];
+		String file = "/Users/matjaz/GitHub Projects/Wumpus/Docs/svet_wumpusa_1.txt";
 		
 		if(!file.isEmpty()){
 			try{
